4d15702d070bd7df2c5017601ed6d312837dcf59
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-05-18 11:55:54 +0900

    chore: configure Storybook and upgrade Next.js

--- src/components/Counter/index.test.tsx
+++ src/components/Counter/index.test.tsx
@@ -1,6 +1,6 @@
 import Counter from './';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { userEvent } from '@testing-library/user-event';
 
 // utility queries
 function getDisplayValue(expectedValue: string) {
