d34c879a9a612385ff6e12b01986c09b6ce76b7f
Yuki Yamamura <yuki.yamamura@icloud.com>
2024-05-20 06:29:22 +0900

    chore: learn how to overwrite mock handlers in a story

--- src/components/PostList/index.stories.tsx
+++ src/components/PostList/index.stories.tsx
@@ -1,11 +1,20 @@
 import PostList from '.';
 import { handlers } from '@/mocks/handlers';
 import { expect, within } from '@storybook/test';
+import { http, HttpResponse } from 'msw';
+import { SWRConfig } from 'swr';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
   component: PostList,
+  decorators: [
+    (Story) => (
+      <SWRConfig value={{ provider: () => new Map() }}>
+        <Story />
+      </SWRConfig>
+    ),
+  ],
   parameters: {
     layout: 'centered',
     msw: {
@@ -17,6 +26,25 @@ const meta = {
 
 type Story = StoryObj<typeof PostList>;
 
+export const FailurePattern: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get('https://jsonplaceholder.typicode.com/posts', () => {
+          return HttpResponse.json(null, { status: 403 });
+        }),
+      ],
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const errorMessage = await canvas.findByText(/something went wrong/i);
+
+    await expect(errorMessage).toBeInTheDocument();
+  },
+};
+
 export const SuccessPattern: Story = {
   play: async ({ canvasElement }) => {
     const postTitles = [
@@ -26,7 +54,7 @@ export const SuccessPattern: Story = {
     ];
     const canvas = within(canvasElement);
 
-    const list = canvas.getByRole('list');
+    const list = await canvas.findByRole('list');
 
     await expect(list).toBeInTheDocument();
     for (const title of postTitles) {
